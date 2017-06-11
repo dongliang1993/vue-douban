9468ed14813ef22ddc27c3deb6a05bf073298580
dongliang <809812556@qq.com>
2017-06-11 13:25:03 +0800

    增加home组件

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <Search @showSearch="controlSearch"></Search>
+  </div>
+</template>
+
+
+<script>
+  import Search from '@/components/Hello.vue'
+  export default {
+    name: 'home',
+    components: {
+      Search
+    },
+    data () {
+      return {
+      }
+    },
+    methods: {
+      controlSearch () {
+        console.log(1)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scope>
+
+</style>
